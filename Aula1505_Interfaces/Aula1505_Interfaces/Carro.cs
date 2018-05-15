d0d11c5728fcd741884cba3b2f90f1670647e601
1712244 <lucasalvesoliva@gmail.com>
2018-05-14 22:21:32 -0300

    Quarta aula de C# onde o prof nos mostrou um pouco sobre interfaces e POLIMORFISMO

---
+++ Aula1505_Interfaces/Aula1505_Interfaces/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1505_Interfaces
+{
+    class Carro : IMeioTransporte
+    {
+        public int Velocidade { get; set; }
+
+        // chave para a classe Marca
+        public int MarcaID { get; set; }
+
+        // instancia para o objeto relacionado, famoso LAZY LOAD, procurar no GOOGLE caso necessario
+        public virtual Marca _Marca { get; set; }
+
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Desacelerar()
+        {
+            Velocidade -= 6;
+        }
+    }
+}
---
+++ Aula1505_Interfaces/Aula1505_Interfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1505_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+
+            Console.WriteLine("Carro criado, velocidade inicial: " + c.Velocidade);
+            c.Acelerar();
+            c.Acelerar();
+            AcelerarBastante(c);
+            Console.WriteLine("Carro, valocidade atual: " + c.Velocidade);
+
+            Onibus o = new Onibus();
+
+            Console.WriteLine("Onibus Criado, velocidade inicial: " + o.Velocidade);
+            o.Acelerar();
+            AcelerarBastante(o);
+            o.Acelerar();
+            o.Desacelerar();
+            Console.WriteLine("Onibus, velocidade atual: " + o.Velocidade);
+
+            Console.ReadKey();
+        }
+
+        static void AcelerarBastante(IMeioTransporte meioTransporte)
+        {
+            for (int  i = 0; i < 10; i++)
+            {
+                meioTransporte.Acelerar();
+            }
+        }
+    }
+}
